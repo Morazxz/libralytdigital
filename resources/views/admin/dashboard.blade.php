4d536fcb5f993a762c0962e3732125394646e4b7
macha06 <fathurrahmansidiq097@gmail.com>
2024-02-22 11:56:32 +0700

    libraly

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,14 +4,8 @@
         <a href="#" class="burger-btn d-block d-xl-none">
             <i class="bi bi-justify fs-3"></i>
         </a>
-    </header>
-
-    <div class="page-heading">
-        <h3>halo admin</h3>
-    </div>
-    <div class="page-content">
-        <section class="row">
-            <div class="col-12 col-lg-12">
+        <div class="card">
+            <div class="card-body">
                 <table class="table">
                     <thead>
                       <tr>
@@ -41,6 +35,16 @@
                       </tr>
                     </tbody>
                   </table>
+            </div>
+          </div>
+    </header>
+
+    <div class="page-heading">
+        <h3>halo admin</h3>
+    </div>
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 col-lg-12">
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
